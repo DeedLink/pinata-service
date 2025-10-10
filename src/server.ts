bbb5f21d67982de6b0e7fb0a50fc1b3c60fa9fa5
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-10 11:00:07 +0530

    public accessability added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 import app from "./app";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`🚀 Pinata URI Service running on port ${PORT}`));
+const PORT = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Pinata URI Service running on port ${PORT}`);
+});
