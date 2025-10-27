c2b6cf1dd75376a97bd0fb3e8fe4752ed1bb11cb
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-27 19:01:37 +0530

    f

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from "./app";
 
 dotenv.config();
 
-if (!process.env.VERCEL) {
+if (!process.env.VERCEL && process.env.NODE_ENV !== 'production') {
   const PORT = Number(process.env.PORT) || 5000;
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Pinata URI Service running on port ${PORT}`);
